67475e0e961ccb2f41b9da3c740ca2934f9c9e8c
JBleidorn <jbleidorn@gmx.de>
2019-10-29 17:00:25 +0100

    dabdjk

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_basis_neu_app/screens/beagle_screen.dart';
+import 'package:flutter_basis_neu_app/screens/havanneser_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -8,6 +10,33 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+          leading: Icon(Icons.menu),
+          title: Text("Home"),
+          actions: <Widget>[
+            Icon(Icons.pregnant_woman),
+          ]),
+      body: Center(
+        child: Column(children: <Widget>[
+          buildRaisedHunderasseButton(context, "Havaneser", HavaneserScreen()),
+          buildRaisedHunderasseButton(context, "Beagle", BeagleScreen()),
+        ]),
+      ),
+    );
+  }
+
+  Widget buildRaisedHunderasseButton(BuildContext context, text, widget) {
+    return RaisedButton(
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => widget,
+          ),
+        );
+      },
+      child: Text(text),
+    );
   }
 }
